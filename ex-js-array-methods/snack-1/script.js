b02fbc4b1a36c7e1fe44e16bdbca41b32820a4d5
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-13 15:28:16 +0100

    first commit

---
+++ ex-js-array-methods/snack-1/script.js
@@ -0,0 +1,12 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// METODO FOR EACH 
+
+names.forEach(function(name){
+    console.log(name);
+    
+})
